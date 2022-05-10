98e0ab19487b3bda46efa393080a6cc390abaa6a
Karol <karol.kopacz@wp.pl>
2022-05-10 13:02:46 +0200

    Poprawka ValidateUserData

--- src/main/java/com/example/workflow/mvc/delegates/ValidateUserData.java
+++ src/main/java/com/example/workflow/mvc/delegates/ValidateUserData.java
@@ -1,5 +1,6 @@
 package com.example.workflow.mvc.delegates;
 
+import com.example.workflow.mvc.entity.Client;
 import com.example.workflow.mvc.entity.LoanOptions;
 import com.example.workflow.mvc.service.ClientService;
 import org.camunda.bpm.engine.RuntimeService;
@@ -7,6 +8,8 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ValidateUserData implements JavaDelegate {
@@ -20,6 +23,16 @@ public class ValidateUserData implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
         System.out.println("delegate: ValidateUserData");
+
+        Long userIdentificationNumber = (Long) delegateExecution.getVariable("userIdentificationNumber");
+        String firstName = (String) delegateExecution.getVariable("firstName");
+        String lastName = (String) delegateExecution.getVariable("lastName");
+        String currency = (String) delegateExecution.getVariable("currency");
+        String street = (String) delegateExecution.getVariable("street");
+        String phoneNumber = (String) delegateExecution.getVariable("phoneNumber");
+        String declaredIncome = (String) delegateExecution.getVariable("declaredIncome");
+
+        Optional<Client> client = clientService.findCientById(userIdentificationNumber);
 
         String loanOption = (String) delegateExecution.getVariable("loanOption");
 
