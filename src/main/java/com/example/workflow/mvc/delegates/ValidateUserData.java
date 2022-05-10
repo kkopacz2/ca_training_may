1c128b3f3926101a04023717786431338f6f7b1f
NobleProg <a@b.com>
2022-05-10 14:24:02 +0200

    zadanie 2.2, external service task

--- src/main/java/com/example/workflow/mvc/delegates/ValidateUserData.java
+++ src/main/java/com/example/workflow/mvc/delegates/ValidateUserData.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
-import java.util.Optional;
 
 @Service
 public class ValidateUserData implements JavaDelegate {
@@ -24,34 +23,37 @@ public class ValidateUserData implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
         System.out.println("delegate: ValidateUserData");
-        Long userIdentificationNumber = (Long) delegateExecution.getVariable("userIdentificationNumber");
-        String loanOption = (String) delegateExecution.getVariable("loanOption");
-        String currency = (String) delegateExecution.getVariable("currency");
-        String street = (String) delegateExecution.getVariable("street");
-        Long phoneNumber = (Long) delegateExecution.getVariable("phoneNumber");
-        String declaredIncome = (String) delegateExecution.getVariable("declaredIncome");
-//        String firstName = (String) delegateExecution.getVariable("firstName");
-//        String lastName = (String) delegateExecution.getVariable("lastName");
-
-
-        if (isLoanOptionInvalid(delegateExecution, loanOption)) return;
-
-        Optional<Client> optClient = clientService.findCientById(userIdentificationNumber);
-
-        if (optClient.isPresent()) {
-            Client client = optClient.get();
-            if (!isCustomerCorrect(client, currency, street, phoneNumber, declaredIncome)) {
-                delegateExecution.setVariable("isDataValid", false);
-                return;
-            }
-            System.out.println("Customer is correct in db.");
-        } else {
-            System.out.println("Customer doesn't exist in db. Assuming it is not ok");
-            delegateExecution.setVariable("isDataValid", false);
-            return;
-        }
-
         delegateExecution.setVariable("isDataValid", true);
+        return;
+
+//        Long userIdentificationNumber = (Long) delegateExecution.getVariable("userIdentificationNumber");
+//        String loanOption = (String) delegateExecution.getVariable("loanOption");
+//        String currency = (String) delegateExecution.getVariable("currency");
+//        String street = (String) delegateExecution.getVariable("street");
+//        Long phoneNumber = (Long) delegateExecution.getVariable("phoneNumber");
+//        String declaredIncome = (String) delegateExecution.getVariable("declaredIncome");
+////        String firstName = (String) delegateExecution.getVariable("firstName");
+////        String lastName = (String) delegateExecution.getVariable("lastName");
+//
+//
+//        if (isLoanOptionInvalid(delegateExecution, loanOption)) return;
+//
+//        Optional<Client> optClient = clientService.findCientById(userIdentificationNumber);
+//
+//        if (optClient.isPresent()) {
+//            Client client = optClient.get();
+//            if (!isCustomerCorrect(client, currency, street, phoneNumber, declaredIncome)) {
+//                delegateExecution.setVariable("isDataValid", false);
+//                return;
+//            }
+//            System.out.println("Customer is correct in db.");
+//        } else {
+//            System.out.println("Customer doesn't exist in db. Assuming it is not ok");
+//            delegateExecution.setVariable("isDataValid", false);
+//            return;
+//        }
+//
+//        delegateExecution.setVariable("isDataValid", true);
     }
 
     private boolean isCustomerCorrect(Client client, String currency, String street, Long phoneNumber, String declaredIncome) {
