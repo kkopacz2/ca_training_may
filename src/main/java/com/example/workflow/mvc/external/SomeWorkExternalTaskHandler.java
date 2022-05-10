e6f615076ad62456b357a916c0dca9edbaa859e5
unknown <tomasz.feliks.mlynarczyk@gmail.com>
2022-05-10 12:05:18 +0200

    external tasks

--- src/main/java/com/example/workflow/mvc/external/SomeWorkExternalTaskHandler.java
+++ src/main/java/com/example/workflow/mvc/external/SomeWorkExternalTaskHandler.java
@@ -1,2 +1,18 @@
-package com.example.workflow.mvc.external;public class SomeWorkExternalTaskHandler {
+package com.example.workflow.mvc.external;
+
+import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.client.task.ExternalTaskHandler;
+import org.camunda.bpm.client.task.ExternalTaskService;
+import org.springframework.stereotype.Component;
+
+@Component
+@ExternalTaskSubscription("doSomeWork")
+public class SomeWorkExternalTaskHandler implements ExternalTaskHandler {
+
+    @Override
+    public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
+        System.out.println("Started external task");
+        externalTaskService.complete(externalTask);
+    }
 }
--- src/main/java/com/example/workflow/mvc/processes/PizzaProcess.java
+++ src/main/java/com/example/workflow/mvc/processes/PizzaProcess.java
@@ -1,2 +1,8 @@
-package com.example.workflow.mvc.processes;public class PizzaProcess {
+package com.example.workflow.mvc.processes;
+
+public class PizzaProcess {
+
+    public static String PROCESS_ID = "PR_01_PIZZA_CHOICE";
+    public static String PR_01_ST_001_CORRELLATE = "PR_01_ST_001_CORRELLATE";
+    public static String PR_01_UT_001_SELECT_THICK = "PR_01_UT_001_SELECT_THICK";
 }
