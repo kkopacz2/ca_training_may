e0622d54809b3a4108bf0559bff3e7ee532d4329
unknown <tomasz.feliks.mlynarczyk@gmail.com>
2022-05-11 09:38:21 +0200

    additional tests

--- src/test/java/com/example/workflow/SimpleProcessTest.java
+++ src/test/java/com/example/workflow/SimpleProcessTest.java
@@ -1,11 +1,14 @@
 package com.example.workflow;
 
+import com.example.workflow.mvc.delegates.MockDelegate;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests;
 import org.camunda.bpm.spring.boot.starter.test.helper.AbstractProcessEngineRuleTest;
+import org.camunda.community.mockito.DelegateExpressions;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
@@ -13,11 +16,16 @@ import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
 @Deployment(resources = {"processes/diagram_3.bpmn"})
 public class SimpleProcessTest extends AbstractProcessEngineRuleTest {
 
+    @Mock
+    MockDelegate mockDelegate;
 
     @Test
     public void shouldExecuteHappyPath() {
         // given
         MockitoAnnotations.openMocks(this);
+
+        DelegateExpressions.registerJavaDelegateMock("mockDelegate").
+                onExecutionSetVariable("key", "value");
 
         ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("PR_003_USER_TASK");
 
@@ -29,8 +37,10 @@ public class SimpleProcessTest extends AbstractProcessEngineRuleTest {
         Task task = taskService().createTaskQuery().processInstanceId(processInstance.getId()).singleResult();
 
         BpmnAwareTests.complete(task);
-
         assertThat(processInstance).hasPassed("FirstTask");
+        task = taskService().createTaskQuery().processInstanceId(processInstance.getId()).singleResult();
+        BpmnAwareTests.complete(task);
+        assertThat(processInstance).hasPassed("SecondTask");
         assertThat(processInstance).isEnded();
 
     }
